ab19d405a1ed66c4d3eedd4cf358e70e54de1f82
Ovchinnikov Danila <52536804+Danik-Off@users.noreply.github.com>
2023-10-17 20:38:55 +0300

    update

--- messangerClient/src/App.tsx
+++ messangerClient/src/App.tsx
@@ -1,15 +1,11 @@
 import Header from "./components/header";
 import "./App.scss";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ChatPage from "./pages/chat-page";
 import ErrorPage from "./pages/error-page";
 import SettingsPage from "./pages/settings-page";
 
 function App() {
-
   const router = createBrowserRouter([
     {
       path: "/",
@@ -24,12 +20,12 @@ function App() {
   if (document.documentElement.requestFullscreen) {
     document.documentElement.requestFullscreen();
   }
-  
+
   return (
     <div className="container">
       <Header></Header>
       <div className="page-container">
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
       </div>
     </div>
   );
